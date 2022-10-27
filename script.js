025fb3609dc4c181d03d0735f1a1b7620f4f04a5
ardamoin <ardamoin2@gmail.com>
2022-10-27 19:57:29 -0400

    Add slider component to html and js + style changes

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const containerDiv = document.querySelector(".container");
+const containerSideLength = 500;
+let numberOfSquares = 16;
+let height = containerSideLength / numberOfSquares;
+let width = containerSideLength / numberOfSquares;
+const myRange = document.querySelector("#my-range");
+const rangeValue = document.querySelector('.range-value');
 
-
-containerDiv.addEventListener('mousedown', () => clicking = true);
+myRange.addEventListener('input', function () {
+    numberOfSquares = myRange.value;
+    rangeValue.textContent = myRange.value;
+})
 
 
 function changeColor(e) {
@@ -10,13 +18,17 @@ function changeColor(e) {
 
 
 
-for (let r = 0; r < 16; r++) {
+
+for (let r = 0; r < numberOfSquares; r++) {
     const row = document.createElement("div");
     row.classList.add("row");
     
-    for (let c = 0; c < 16; c++) {
+    for (let c = 0; c < numberOfSquares; c++) {
+
         const column = document.createElement("div");
         column.classList.add("column");
+        column.style.height = `${height}px`;
+        column.style.width = `${width}px`;
 
         column.addEventListener('mousedown', changeColor);
         row.appendChild(column);
