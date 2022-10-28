d9a107e88a3c252fc1d7a5d19af46dccbb042d4f
ardamoin <ardamoin2@gmail.com>
2022-10-28 18:35:14 -0400

    Add two functions: makeGrid() and changeSize().
    makeGrid makes it easier to create a new grid. It is called
    once to initialize the grid.
    
    changeSize is to change the number of boxes contained in the grid.
    Once changeSize is called, variables needed to create the grid
    are re-assigned, all child nodes in the container div are deleted
    and a new grid is initialized using makeGrid function.

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ let width = containerSideLength / numberOfSquares;
 const myRange = document.querySelector("#my-range");
 const rangeValue = document.querySelector('.range-value');
 
-myRange.addEventListener('input', function () {
-    numberOfSquares = myRange.value;
-    rangeValue.textContent = myRange.value;
-})
+myRange.value = numberOfSquares;
+rangeValue.textContent = `${myRange.value} x ${myRange.value}`;
+
+myRange.addEventListener('input', changeSize);
 
 
 function changeColor(e) {
@@ -17,22 +17,39 @@ function changeColor(e) {
 }
 
 
+function changeSize() {
+    numberOfSquares = myRange.value;
+    rangeValue.textContent = `${myRange.value} x ${myRange.value}`;
+    height = containerSideLength / numberOfSquares;
+    width = containerSideLength / numberOfSquares;
 
+    for (const node of containerDiv.querySelectorAll("*")) {
+        node.remove();
+    } // to remove old grid
 
-for (let r = 0; r < numberOfSquares; r++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    
-    for (let c = 0; c < numberOfSquares; c++) {
+    makeGrid();
 
-        const column = document.createElement("div");
-        column.classList.add("column");
-        column.style.height = `${height}px`;
-        column.style.width = `${width}px`;
-
-        column.addEventListener('mousedown', changeColor);
-        row.appendChild(column);
-    }   
-
-    containerDiv.appendChild(row);
 }
+
+
+function makeGrid() {
+    for (let r = 0; r < numberOfSquares; r++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        
+        for (let c = 0; c < numberOfSquares; c++) {
+    
+            const column = document.createElement("div");
+            column.classList.add("column");
+            column.style.height = `${height}px`;
+            column.style.width = `${width}px`;
+    
+            column.addEventListener('mousedown', changeColor);
+            row.appendChild(column);
+        }   
+    
+        containerDiv.appendChild(row);
+    }
+}
+
+makeGrid(); // to create the initial grid
